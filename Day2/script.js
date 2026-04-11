e36339e7d5e6af297d7a5a132170d95b8225b800
Harshitha0599 <harshithabodhu09@gmal.com>
2026-04-11 09:27:05 +0530

    added day2

---
+++ Day2/script.js
@@ -0,0 +1,21 @@
+/*var a=10;
+var a=20;
+console.log(a);
+
+const d=11111;
+console.log(d);
+
+let c=20;
+c=30;
+console.log(c);*/
+
+let arr=["java",8,null,true,-2.56];
+//console.log(arr[0]);
+
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    console.log(typeof(arr[i]));
+}
+ arr.push("python");
+arr.pop();
+
---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+var a=20
+var b="20"
+
+if(a === b)
+    console.log("equal");
+else
+    console.log("not equal");
+
+console.log(2 ** 4);
+
+var a=10;
+
+for(var i=1;i<=a;i++){
+    var fc=0;
+    for(var j=1;j<=i;j++){
+        if(i%j==0)
+            fc++;
+    }
+    if(fc==2)
+        console.log(i);
+}
+
+var a=10;
+var a=25;
+console.log(a);
+
+let c=20;
+c=59;
+console.log(c);
+
+const d=10000;
+console.log(d);
