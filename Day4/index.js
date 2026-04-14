5da93122334ff06fbc923ea6ac993ca26b203871
Harshitha0599 <harshithabodhu09@gmal.com>
2026-04-14 09:44:53 +0530

    modified

--- Day4/index.js
+++ Day4/index.js
@@ -1,7 +1,7 @@
 
 //Dom in js
 
-let x = document.getElementById("id1");
+/*let x = document.getElementById("id1");
 
 console.log(x);
 console.log(x.innerText);
@@ -9,6 +9,25 @@ console.log(x.innerText);
 
 x.innerText = "dom in js";
 
-x.innerHTML = "<b>DOM IN JS</b>";
+x.innerHTML = "<b>DOM IN JS</b>";*/
+
+let y = document.getElementsByClassName("class1"); 
+
+console.log(y);
+
+let z = document.getElementsByClassName("class2");
+
+let a = document.getElementsByClassName("class3");
+
+console.log(z);
+console.log(a);
+
+z[0].innerHTML = "<b>hello</b>";
+
+y[0].innerHTML = "<b>Dom in js</b>";
+
+a[2].innerHTML = "<b>box 3</b>";
+
+a[1].innerHTML = "<b>box 2</b>";
 
 
